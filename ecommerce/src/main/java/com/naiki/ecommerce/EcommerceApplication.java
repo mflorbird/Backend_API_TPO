b30d967c8588fe7985a3fd5c2904bc8ca8179bcd
Diego B <dbari@seidoranalytics.com>
2024-08-13 22:08:57 -0300

    Update EcommerceApplication.java

--- ecommerce/src/main/java/com/naiki/ecommerce/EcommerceApplication.java
+++ ecommerce/src/main/java/com/naiki/ecommerce/EcommerceApplication.java
@@ -1,4 +1,4 @@
-package com.naiki.ecommerce.impl;
+package com.naiki.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
