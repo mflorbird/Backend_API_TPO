d1502566632e15af33fa731b45effc0714d8d9a9
florbird <mabird@uade.edu.ar>
2024-08-27 20:38:05 -0300

    Create_class_Service_Repository

---
+++ ecommerce/src/main/java/com/naiki/ecommerce/repository/RegisterRepository.java
@@ -0,0 +1,4 @@
+package main.java.com.naiki.ecommerce.repository;
+
+public class RegisterRepository {
+}
---
+++ ecommerce/src/main/java/com/naiki/ecommerce/service/RegisterService.java
@@ -0,0 +1,4 @@
+package main.java.com.naiki.ecommerce.service;
+
+public class RegisterService {
+}
