ed7acb078bdccf1a69cfb914fe3951895429e1c6
Diego B <dbari@seidoranalytics.com>
2024-08-27 20:13:48 -0300

    primer commit con controller, service y repo

---
+++ ecommerce/src/main/java/com/naiki/ecommerce/controllers/CatalogoProductosController.java
@@ -0,0 +1,58 @@
+package com.naiki.ecommerce.controllers;
+
+import com.naiki.ecommerce.service.CatalogoProductosService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("api/v1/gestionCatalogo")
+public class CatalogoProductosController {
+
+    @Autowired
+    private CatalogoProductosService service;
+
+    // Productos destacados
+    @RequestMapping("/productosDestacados")
+    public String getProductosDestacados() {
+        return service.getProductosDestacados();
+    }
+
+    // Productos por categoria
+    @GetMapping("/productosPorCategoria/{categoria}")
+    public String getProductosPorCategoria(String categoria) {
+        return service.getProductosPorCategoria(categoria);
+    }
+
+    // Productos recientes vistos por el usuario
+    @RequestMapping("/productosRecientes")
+    public String getProductosRecientes() {
+        return service.getProductosRecientes();
+    }
+
+    // Detalle de un producto
+    @GetMapping("/detalleProducto/{productoId}")
+    public String getDetalleProducto(String productoId) {
+        return service.getDetalleProducto(productoId);
+    }
+
+    // Revisar stock de un producto
+    @GetMapping("/stockProducto/{productoId}")
+    public String getStockProducto(String productoId) {
+        return service.getStockProducto(productoId);
+    }
+
+    // Agregar un producto al carrito (Ver si se superpone)
+    @GetMapping("/agregarProductoAlCarrito/{productoId}")
+    public String agregarProductoAlCarrito(String productoId) {
+        return service.agregarProductoAlCarrito(productoId);
+    }
+
+    // Agregar a Favoritos del usuario
+    @GetMapping("/agregarProductoAFavoritos/{userId}/{productoId}")
+    public String agregarProductoAFavoritos(String userId, String productoId) {
+        return service.agregarProductoAFavoritos(userId, productoId);
+    }
+
+}
