1bad534e1db7d27aaae0b13bfdeb2af9d1edb2bc
florbird <mabird@uade.edu.ar>
2024-09-14 15:32:53 -0300

    Update UserController.java

--- ecommerce/src/main/java/com/naiki/ecommerce/controllers/user/UserController.java
+++ ecommerce/src/main/java/com/naiki/ecommerce/controllers/user/UserController.java
@@ -16,51 +16,5 @@ public class UserController {
         return ResponseEntity.ok(user);
     }
 
-
-
-
-//    private final UserService userService;
-//    private final AuthenticationManager authenticationManager;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public UserController(UserService userService, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
-//        this.userService = userService;
-//        this.authenticationManager = authenticationManager;
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    // Endpoint para registrar un nuevo usuario
-//    @PostMapping("/register")
-//    public ResponseEntity<String> registerUser(@RequestBody User user) {
-//        try {
-//            userService.registerUser(user);
-//            return new ResponseEntity<>("Usuario registrado exitosamente", HttpStatus.CREATED);
-//        } catch (RuntimeException e) {
-//            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-//        }
-//    }
-//
-//    // Endpoint para hacer login
-//    @PostMapping("/login")
-//    public ResponseEntity<String> loginUser(@RequestParam("usernameOrEmail") String usernameOrEmail,
-//                                            @RequestParam("password") String password) {
-//        try {
-//            // Autenticar al usuario
-//            Authentication authentication = authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(usernameOrEmail, password)
-//            );
-//
-//            // Si la autenticación es exitosa, guardamos el contexto de seguridad
-//            SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-//            return new ResponseEntity<>("Login exitoso para el usuario: " + userDetails.getUsername(), HttpStatus.OK);
-//        } catch (Exception e) {
-//            return new ResponseEntity<>("Nombre de usuario o contraseña incorrectos", HttpStatus.UNAUTHORIZED);
-//        }
-//    }
-
-
 }
 
