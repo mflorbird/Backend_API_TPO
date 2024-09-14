354037ea55e80a5891e6ef94710c22445d4ceee0
NATESTA13 <natesta@uade.edu.ar>
2024-09-14 10:34:37 -0300

    ItemCarrito may to oone : relacin de muchos a uno con el carro. varios items en un carro.

---
+++ ecommerce/src/main/java/com/naiki/ecommerce/entity/ItemCarrito.java
@@ -0,0 +1,53 @@
+import javax.persistence.*;
+import com.naiki.ecommerce.repositoy.entity;
+
+@Entity
+public class ItemCarrito{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="carrito_id")
+    private Carrito carrito;
+
+    @ManyToOne
+    @JoinColumn(name="producto_id")
+    private Producto producto;
+
+    private int cantidad;
+
+    //get y set
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+}
