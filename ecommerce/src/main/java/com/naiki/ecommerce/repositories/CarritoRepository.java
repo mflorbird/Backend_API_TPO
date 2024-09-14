e3c2eb9df7ad3f3f6e03ff9be27a52dab2abefa3
NATESTA13 <natesta@uade.edu.ar>
2024-09-14 12:12:43 -0300

    JPA es la interfaz que tiene los metodos crear leer actualizar eliminar. long porque en carrito esta el id long.

---
+++ ecommerce/src/main/java/com/naiki/ecommerce/repositories/CarritoRepository.java
@@ -0,0 +1,12 @@
+import com.naiki.ecommerce.repository.entity.Carrito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarritoRepository extends JpaRepository <Carrito, Long>{
+}
+
+//findAll(): obtener carritos
+//findById(long id): busca el carrito x ID
+//save(Carrito ccarito): guarda o atualizael carro en BD
+//DELETE(Carrito carrito): elimina carrito
