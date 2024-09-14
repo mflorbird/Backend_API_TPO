f8519cf3a5031454ea3b835a05b49f6c5047d000
NATESTA13 <natesta@uade.edu.ar>
2024-09-14 10:10:26 -0300

    Carrito > Entidad OneToMany esta es la relacion de uno a muchos (un carrito puede tener muchos productos).

--- ecommerce/src/main/java/com/naiki/ecommerce/entity/Carrito.java
+++ ecommerce/src/main/java/com/naiki/ecommerce/entity/Carrito.java
@@ -10,6 +10,35 @@ public class Carrito{
     @GeneratedValue(strategy=GenerationType.IDENTITY) // PARA GENERAR SOLO EL ID
     private long id;
 
+    @OneToMany
+    public List<ItemCarrito> items= new ArrayList<>(); //lista de productos en el carro
+    private double totalPrecio=0.0; //este es el precio del carro
+
+    // get y set
+
+    public long getId() {
+        return id;
+    }
+
+    public List<ItemCarrito> getItems() {
+        return items;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setItems(List<ItemCarrito> items) {
+        this.items = items;
+    }
+
+    public double getTotalPrecio() {
+        return totalPrecio;
+    }
+
+    public void setTotalPrecio(double totalPrecio) {
+        this.totalPrecio = totalPrecio;
+    }
 }
 
 
