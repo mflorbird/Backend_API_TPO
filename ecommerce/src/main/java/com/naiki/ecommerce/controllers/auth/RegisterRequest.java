e4c914ae849dfbed2d67e8fd2bef631a0ccf045e
florbird <mabird@uade.edu.ar>
2024-09-15 21:04:47 -0300

    Login_Register_V5.0
    Fix registro y login.
    - Los usuarios se registran de forma exitosa (no da error 403)
    - Los usuarios se autentican de forma exitosa

--- ecommerce/src/main/java/com/naiki/ecommerce/controllers/auth/RegisterRequest.java
+++ ecommerce/src/main/java/com/naiki/ecommerce/controllers/auth/RegisterRequest.java
@@ -16,6 +16,7 @@ public class RegisterRequest {
     private String firstname;
     private String lastname;
     private String email;
+    private String username;
     private String password;
     private LocalDate birthDate;
     private Role role;
